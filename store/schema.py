87d433b0dc6e9c670eed5d682708c9d8ce5a4986
Preethi-ghub <Preethigunasekar1@gmail.com>
2025-07-17 13:28:01 +0530

    Implemented Product GraphQL query with filters for category and inStock

--- store/schema.py
+++ store/schema.py
@@ -2,29 +2,31 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from .models import Product
 
-# Define the GraphQL type for Product
+
 class ProductType(DjangoObjectType):
+    inStock = graphene.Boolean(source='in_stock')
+
     class Meta:
         model = Product
         fields = "__all__"
 
-    inStock = graphene.Boolean(source='in_stock') 
-
-# Define the Query
 class Query(graphene.ObjectType):
     products = graphene.List(
         ProductType,
         category=graphene.String(required=False),
-        in_stock=graphene.Boolean(required=False)
+        inStock=graphene.Boolean(required=False)
     )
 
-    def resolve_products(self, info, category=None, in_stock=None):
+    def resolve_products(self, info, category=None, inStock=None):
         products = Product.objects.all()
+
         if category:
-            products = products.filter(category=category)
-        if in_stock is not None:
-            products = products.filter(inStock=in_stock)
+            products = products.filter(category__iexact=category)
+
+        if inStock is not None:
+            products = products.filter(in_stock=inStock)  
         return products
+
 
 schema = graphene.Schema(query=Query)
 
